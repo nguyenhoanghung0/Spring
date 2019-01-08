6858fb22da6c177d51edc98a45ce9033707c111f
nguyenhoanghung0 <46061500+nguyenhoanghung0@users.noreply.github.com>
2019-01-09 00:42:24 +0800

    Change project setup

--- vehicleTracking/src/main/java/com/spring/vehicletracking/Application.java
+++ vehicleTracking/src/main/java/com/spring/vehicletracking/Application.java
@@ -4,15 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.spring.vehicletracking")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) {
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+	
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
                 
     }    
